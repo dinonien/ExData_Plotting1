645733ffe6793c16d4aa5683f45c9bba2f6c4372
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-02-25 10:51:05 -0500

    Plot1.R created and README.md changed.

---
+++ Plot1.R
@@ -0,0 +1,11 @@
+###########################################################
+## Coursera : Exploratory Data Analysis
+## Course Project 1
+## Author: Dino N.
+## Filename. Plot1.R
+###########################################################
+
+# This code loads dataset "Electric Power Consumption" available from UC Irvine Machine Learning Repository 
+# and plots Histogramm Global Active Power
+# Make sure 
+
